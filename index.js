a6b27886d5b33771d45e5ae9066782dec7da13b5
MohammadHassan Haghighifard <mhideadesign@gmail.com>
2022-10-29 16:36:34 +0330

    timeout

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+var timeout = require('connect-timeout')
 const router = require('./routes')
 const app = express()
+app.use(timeout('30s'))
 const port = 3000
 
 app.use('/', router)
