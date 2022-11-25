d54894f0145011ac02245c1a6be6731a66932152
MohammadHassan Haghighifard <mhideadesign@gmail.com>
2022-11-25 19:41:22 +0330

    change

--- routes.js
+++ routes.js
@@ -1,27 +1,34 @@
+const { time } = require('console')
 const express = require('express')
 const router = express.Router()
 const fs = require('fs')
-const m = require('mathjs')
+
+const fibb = function (n) {
+    if (n == 1 || n == 2) {
+        return 1
+    }
+    return fibb(n - 1) + fibb(n - 2)
+}
+const write = function () {
+    const filename = "./temp/" + (new Date()).getMilliseconds() + "-" + Math.random()
+    for (let i = 0; i < 50; i++) {
+        fs.appendFileSync(filename, 'Hey there!\n');
+    }
+    fs.rmSync(filename);
+}
 // Home
 router.get('/', (req, res) => {
     res.send("Home")
 })
 // define the cpu-intensive route
 router.get('/cpu', (req, res) => {
-    for (let i = 0; i < 1; i++) {
-        let mr = m.random([100, 100])
-        m.inv(mr)
-    }
-    res.send("Done.")
+    fibb(35)
+    res.send("Node finished.")
 })
 // define the io-intensive route
 router.get('/io', (req, res) => {
-    const filename = "./temp/file" + Math.random() * 10000
-    for (let i = 0; i < 10; i++) {
-        fs.appendFileSync(filename, 'Hey there!');
-    }
-    //fs.rmSync(filename);
-    res.send('io')
+    write()
+    res.send("Node finished.")
 })
 
 module.exports = router
